537a29eff0f9a7d39f5ff8cfbf4fd74d44372940
inyeong <135231946+inyeongjang@users.noreply.github.com>
2025-10-10 11:48:40 +0900

    [Silver V] Title: 영화감독 숌, Time: 268 ms, Memory: 86528 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 0; 
+        int num = 665; 
+
+        while (true) {
+            num++;
+            if (String.valueOf(num).contains("666")){
+                count++;
+                if (count == N) {
+                    System.out.println(num);
+                    break;
+                }
+            }
+        }
+    }
+}

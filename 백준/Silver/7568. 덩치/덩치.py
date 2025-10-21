339358f53d60fd93ee08c59e54d99be44ae8d4d7
inyeong <135231946+inyeongjang@users.noreply.github.com>
2025-10-21 21:56:34 +0900

    [Silver V] Title: 덩치, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -5,9 +5,6 @@ for i in range(N):
     x, y = map(int, input().split())
     people.append((x, y))
 
-prev_weight = people[0][0]
-prev_height = people[0][1] 
-
 for (w, h) in people:
     rank = 1
     for (a, b) in people:

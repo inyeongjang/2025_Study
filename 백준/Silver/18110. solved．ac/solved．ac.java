3a924c8d58502529dc26bc7172ad762e4e9daa12
inyeong <135231946+inyeongjang@users.noreply.github.com>
2025-11-22 18:32:08 +0900

    [Silver IV] Title: solved.ac, Time: 336 ms, Memory: 37440 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -1,32 +1,33 @@
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); 
-        
+        int N = Integer.parseInt(br.readLine());
+
         if (N == 0) {
             System.out.println(0);
             return;
         }
-        
-        List<Integer> list = new ArrayList<>();
+
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(br.readLine()));
-        }
-        
-        Collections.sort(list);
-        
-        int number = (int) Math.round(N * 0.15);
-        int sum = 0;
-        int cnt = 0;
-        for (int j = number; j < N - number; j++){
-            sum += list.get(j);
-            cnt++;
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        System.out.println(Math.round((double)sum / cnt));
+        Arrays.sort(arr);
+
+        int cut = (int) Math.round(N * 0.15);  
+        long sum = 0; 
+
+        for (int i = cut; i < N - cut; i++) {
+            sum += arr[i];
+        }
+
+        int count = N - 2 * cut;
+        long result = Math.round((double) sum / count);
+
+        System.out.println(result);
     }
 }

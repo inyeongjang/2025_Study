db9f078870fe046c372ae7b18ff10e9840c8b9db
inyeong <135231946+inyeongjang@users.noreply.github.com>
2025-11-22 17:28:15 +0900

    [Bronze II] Title: Hashing, Time: 84 ms, Memory: 108384 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,14 @@
+L = int(input())
+S = input() 
+
+hash_dict = {}
+for i in range(26):
+    hash_dict[chr(i + 97)] = i + 1 
+
+H = 0
+i = 0
+for alphabet in S:
+    H += hash_dict[alphabet] * 31 ** i
+    i += 1
+
+print(H)
